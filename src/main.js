00af871872f61d02b98326f9ce7cc15c5c184596
Serhii Shevchenko <sergey.groove.shevchenko@gmail.com>
2025-02-07 22:50:03 +0100

    edit libraries

--- src/main.js
+++ src/main.js
@@ -1,8 +1,5 @@
 import Accordion from 'accordion-js';
-import 'accordion-js/dist/accordion.min.css';
-
 import Swiper from 'swiper';
-import 'swiper/css';
 
 import './js/header.js';
 import './js/hero.js';
