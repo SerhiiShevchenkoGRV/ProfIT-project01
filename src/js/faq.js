ae36fb3dfce64b7de61cbc4899192634b32388a8
Serhii Shevchenko <sergey.groove.shevchenko@gmail.com>
2025-02-08 21:13:23 +0100

    add faq

---
+++ src/js/faq.js
@@ -0,0 +1,13 @@
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
+
+document.addEventListener('DOMContentLoaded', function () {
+  const accordion = new Accordion('.faq', {
+    duration: 400,
+    showMultiple: false,
+    elementClass: 'faq-item',
+    triggerClass: 'faq-btn',
+    panelClass: 'faq-content',
+    activeClass: 'is-open',
+  });
+});
