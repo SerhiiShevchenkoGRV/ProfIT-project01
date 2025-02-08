14f423fb4bd2d219502991d79e1a11c05bfcc327
Marchenko189 <marcenkoo189@gmail.com>
2025-02-08 14:48:51 +0200

    95% about-me

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -1,8 +1,38 @@
 import Accordion from 'accordion-js';
 
+import { Navigation, Keyboard } from 'swiper/modules';
+import Swiper from 'swiper';
+
 
 new Accordion('.js-about-descriprion', {
-  duration: 400,
-  showMultiple: true,
+  duration: 500,
+  showMultiple: false,
   openOnInit: [0],
+});
+
+
+const aboutSwiper = new Swiper('.js-about-swiper', {
+  modules: [Navigation, Keyboard],
+  // speed: 400,
+  loop: true,
+  slidesPerView: 2,
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+    
+    },
+    1440: {
+      slidesPerView: 6,
+    },
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+    pageUpDown: true,
+  },
+  
+});
+
+document.querySelector('.js-swiper-btn').addEventListener('click', e => {
+  aboutSwiper.slideNext();
 });
