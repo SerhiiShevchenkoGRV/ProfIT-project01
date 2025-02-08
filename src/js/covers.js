1624f2552cd69f102eadb8f00d88d8be75d1d820
Serhii Shevchenko <sergey.groove.shevchenko@gmail.com>
2025-02-08 17:46:30 +0100

    add gallery

---
+++ src/js/covers.js
@@ -0,0 +1,44 @@
+const coversArr = [
+  {
+    src1x: '../img/covers/cover01@1x.jpg',
+    src1x: '../img/covers/cover01@2x.jpg',
+  },
+  {
+    src1x: '../img/covers/cover02@1x.jpg',
+    src1x: '../img/covers/cover02@2x.jpg',
+  },
+  {
+    src1x: '../img/covers/cover03@1x.jpg',
+    src1x: '../img/covers/cover03@2x.jpg',
+  },
+  {
+    src1x: '../img/covers/cover04@1x.jpg',
+    src1x: '../img/covers/cover04@2x.jpg',
+  },
+  {
+    src1x: '../img/covers/cover05@1x.jpg',
+    src1x: '../img/covers/cover05@2x.jpg',
+  },
+  {
+    src1x: '../img/covers/cover06@1x.jpg',
+    src1x: '../img/covers/cover06@2x.jpg',
+  },
+  {
+    src1x: '../img/covers/cover07@1x.jpg',
+    src1x: '../img/covers/cover07@2x.jpg',
+  },
+  {
+    src1x: '../img/covers/cover08@1x.jpg',
+    src1x: '../img/covers/cover08@2x.jpg',
+  },
+  {
+    src1x: '../img/covers/cover09@1x.jpg',
+    src1x: '../img/covers/cover09@2x.jpg',
+  },
+  {
+    src1x: '../img/covers/cover10@1x.jpg',
+    src1x: '../img/covers/cover10@2x.jpg',
+  },
+];
+
+const galleryList = document.querySelector('.covers-gallery');
