cdb35da90be062f3664eb984eeaec879e405f0ae
Serhii Shevchenko <sergey.groove.shevchenko@gmail.com>
2025-02-09 19:08:52 +0100

    edit img pathes in covers.js

--- src/js/covers.js
+++ src/js/covers.js
@@ -1,52 +1,73 @@
+import cover01_1x from '../img/covers/cover01@1x.jpg';
+import cover01_2x from '../img/covers/cover01@2x.jpg';
+import cover02_1x from '../img/covers/cover02@1x.jpg';
+import cover02_2x from '../img/covers/cover02@2x.jpg';
+import cover03_1x from '../img/covers/cover03@1x.jpg';
+import cover03_2x from '../img/covers/cover03@2x.jpg';
+import cover04_1x from '../img/covers/cover04@1x.jpg';
+import cover04_2x from '../img/covers/cover04@2x.jpg';
+import cover05_1x from '../img/covers/cover05@1x.jpg';
+import cover05_2x from '../img/covers/cover05@2x.jpg';
+import cover06_1x from '../img/covers/cover06@1x.jpg';
+import cover06_2x from '../img/covers/cover06@2x.jpg';
+import cover07_1x from '../img/covers/cover07@1x.jpg';
+import cover07_2x from '../img/covers/cover07@2x.jpg';
+import cover08_1x from '../img/covers/cover08@1x.jpg';
+import cover08_2x from '../img/covers/cover08@2x.jpg';
+import cover09_1x from '../img/covers/cover09@1x.jpg';
+import cover09_2x from '../img/covers/cover09@2x.jpg';
+import cover10_1x from '../img/covers/cover10@1x.jpg';
+import cover10_2x from '../img/covers/cover10@2x.jpg';
+
 const coversArr = [
   {
-    src1x: '../img/covers/cover01@1x.jpg',
-    src2x: '../img/covers/cover01@2x.jpg',
+    src1x: cover01_1x,
+    src2x: cover01_2x,
     alt: 'Power pulse',
   },
   {
-    src1x: '../img/covers/cover02@1x.jpg',
-    src2x: '../img/covers/cover02@2x.jpg',
+    src1x: cover02_1x,
+    src2x: cover02_2x,
     alt: 'Mimino',
   },
   {
-    src1x: '../img/covers/cover03@1x.jpg',
-    src2x: '../img/covers/cover03@2x.jpg',
+    src1x: cover03_1x,
+    src2x: cover03_2x,
     alt: 'Ukrainian artistry',
   },
   {
-    src1x: '../img/covers/cover04@1x.jpg',
-    src2x: '../img/covers/cover04@2x.jpg',
+    src1x: cover04_1x,
+    src2x: cover04_2x,
     alt: 'Organic vegetables',
   },
   {
-    src1x: '../img/covers/cover05@1x.jpg',
-    src2x: '../img/covers/cover05@2x.jpg',
+    src1x: cover05_1x,
+    src2x: cover05_2x,
     alt: 'Wallet',
   },
   {
-    src1x: '../img/covers/cover06@1x.jpg',
-    src2x: '../img/covers/cover06@2x.jpg',
+    src1x: cover06_1x,
+    src2x: cover06_2x,
     alt: 'Chego jewellery',
   },
   {
-    src1x: '../img/covers/cover07@1x.jpg',
-    src2x: '../img/covers/cover07@2x.jpg',
+    src1x: cover07_1x,
+    src2x: cover07_2x,
     alt: 'Energy flow',
   },
   {
-    src1x: '../img/covers/cover08@1x.jpg',
-    src2x: '../img/covers/cover08@2x.jpg',
+    src1x: cover08_1x,
+    src2x: cover08_2x,
     alt: 'Fruitbox',
   },
   {
-    src1x: '../img/covers/cover09@1x.jpg',
-    src2x: '../img/covers/cover09@2x.jpg',
+    src1x: cover09_1x,
+    src2x: cover09_2x,
     alt: 'English excellence',
   },
   {
-    src1x: '../img/covers/cover10@1x.jpg',
-    src2x: '../img/covers/cover10@2x.jpg',
+    src1x: cover10_1x,
+    src2x: cover10_2x,
     alt: 'Starlight studio',
   },
 ];
