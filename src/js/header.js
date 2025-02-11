1c803857a385e9c81e0e7d12876568d6c733acf7
Julia Tarasenko <tarasenkojul88@gmail.com>
2025-02-11 11:07:48 -0800

    fix scrol

--- src/js/header.js
+++ src/js/header.js
@@ -1,96 +1,3 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//   const refs = {
-//     openMenuBtn: document.querySelector('[data-menu-open]'),
-//     closeMenuBtn: document.querySelector('[data-menu-close]'),
-//     menu: document.querySelector('[data-menu]'),
-//     navLinks: document.querySelectorAll('.mobile-menu-list .mobile-nav-link'),
-//     orderProjectBtnMobile: document.querySelector('.mobile-btn'),
-//     orderProjectBtnDesktop: document.querySelector('.header-btn'),
-//     dropdowns: document.querySelectorAll('.dropdown'),
-//   };
-
-//   if (refs.openMenuBtn && refs.closeMenuBtn && refs.menu) {
-//     refs.openMenuBtn.addEventListener('click', () => {
-//       toggleMenu();
-//     });
-
-//     refs.closeMenuBtn.addEventListener('click', () => {
-//       toggleMenu();
-//     });
-
-//     if (refs.navLinks.length) {
-//       refs.navLinks.forEach(link => {
-//         link.addEventListener('click', () => {
-//           window.location.href = link.getAttribute('href');
-//           closeMenu();
-//         });
-//       });
-//     }
-
-//     // Обробник для мобільної кнопки "Order the project"
-//     if (refs.orderProjectBtnMobile) {
-//       refs.orderProjectBtnMobile.addEventListener('click', () => {
-//         scrollToWorkTogetherSection();
-//         closeMenu();
-//       });
-//     }
-
-//     // Обробник для десктопної кнопки "Order the project"
-//     if (refs.orderProjectBtnDesktop) {
-//       refs.orderProjectBtnDesktop.addEventListener('click', () => {
-//         scrollToWorkTogetherSection();
-//       });
-//     }
-
-//     // Обробники для меню, що випадає
-//     if (refs.dropdowns.length) {
-//       refs.dropdowns.forEach(dropdown => {
-//         dropdown.addEventListener('click', function (event) {
-//           const dropdownContent = this.querySelector('.dropdown-content');
-//           dropdownContent.classList.toggle('show');
-//           event.stopPropagation();
-//         });
-//       });
-//     }
-//   }
-
-//   // Функция для плавного скроллинга к секции "work-together"
-//   function scrollToWorkTogetherSection() {
-//     const workTogetherSection = document.getElementById('work-togeth');
-//     if (workTogetherSection) {
-//       workTogetherSection.scrollIntoView({
-//         behavior: 'smooth',
-//         block: 'start',
-//       });
-//     }
-//   }
-
-//   function toggleMenu() {
-//     refs.menu.classList.toggle('is-open');
-//   }
-
-//   function closeMenu() {
-//     refs.menu.classList.remove('is-open');
-//   }
-
-//   // Закрити меню при зміні ширини екрана (для планшетів та десктоп)
-//   window.addEventListener('resize', () => {
-//     if (window.innerWidth > 767) {
-//       closeMenu();
-//     }
-//   });
-
-//   // Закриття випадаючих меню при кліку поза ними
-//   document.addEventListener('click', function (event) {
-//     refs.dropdowns.forEach(dropdown => {
-//       const dropdownContent = dropdown.querySelector('.dropdown-content');
-//       if (!dropdown.contains(event.target)) {
-//         dropdownContent.classList.remove('show');
-//       }
-//     });
-//   });
-// });
-// =====================================================================================
 document.addEventListener('DOMContentLoaded', () => {
   const refs = {
     openMenuBtn: document.querySelector('[data-menu-open]'),
@@ -105,12 +12,12 @@ document.addEventListener('DOMContentLoaded', () => {
   if (refs.openMenuBtn && refs.closeMenuBtn && refs.menu) {
     refs.openMenuBtn.addEventListener('click', () => {
       toggleMenu();
-      disableBodyScroll(); // Отключаем скролл при открытии меню
+      disableBodyScroll();
     });
 
     refs.closeMenuBtn.addEventListener('click', () => {
       toggleMenu();
-      enableBodyScroll(); // Включаем скролл при закрытии меню
+      enableBodyScroll();
     });
 
     if (refs.navLinks.length) {
@@ -118,7 +25,7 @@ document.addEventListener('DOMContentLoaded', () => {
         link.addEventListener('click', () => {
           window.location.href = link.getAttribute('href');
           closeMenu();
-          enableBodyScroll(); // Включаем скролл при переходе по ссылке
+          enableBodyScroll();
         });
       });
     }
@@ -128,7 +35,7 @@ document.addEventListener('DOMContentLoaded', () => {
       refs.orderProjectBtnMobile.addEventListener('click', () => {
         scrollToWorkTogetherSection();
         closeMenu();
-        enableBodyScroll(); // Включаем скролл при переходе
+        enableBodyScroll();
       });
     }
 
@@ -151,7 +58,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
 
-  // Функция для плавного скроллинга к секции "work-together"
+  // Функція для плавного скролінгу до секції "work-together"
   function scrollToWorkTogetherSection() {
     const workTogetherSection = document.getElementById('work-togeth');
     if (workTogetherSection) {
@@ -170,12 +77,11 @@ document.addEventListener('DOMContentLoaded', () => {
     refs.menu.classList.remove('is-open');
   }
 
-  // Функция для отключения скроллинга на body
+  // Функція для відключення скролінгу на body
   function disableBodyScroll() {
     document.body.classList.add('no-scroll');
   }
 
-  // Функция для включения скроллинга на body
   function enableBodyScroll() {
     document.body.classList.remove('no-scroll');
   }
@@ -184,7 +90,7 @@ document.addEventListener('DOMContentLoaded', () => {
   window.addEventListener('resize', () => {
     if (window.innerWidth > 767) {
       closeMenu();
-      enableBodyScroll(); // Включаем скролл при изменении размера
+      enableBodyScroll();
     }
   });
 
