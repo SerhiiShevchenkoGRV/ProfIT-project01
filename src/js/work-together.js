96c082bfc5137f494f3a8e105c84de6ed0f73d84
Serhii Shevchenko <sergey.groove.shevchenko@gmail.com>
2025-02-12 18:14:12 +0100

    edit footer js

--- src/js/work-together.js
+++ src/js/work-together.js
@@ -20,8 +20,9 @@ form.addEventListener('submit', async function (event) {
   event.preventDefault();
   const email = emailInput.value.trim();
   const comments = commentsInput.value.trim();
+  const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
-  // add empty field validation
+  // Empty inputs validation
   if (!email || !comments) {
     validationText.textContent = 'Fields cannot be empty!';
     validationText.classList.add('error');
@@ -31,40 +32,41 @@ form.addEventListener('submit', async function (event) {
     return;
   }
 
-  if (emailInput.validity.valid) {
-    validationText.classList.remove('error');
-    validationText.textContent = 'Success!';
-    validationText.classList.add('success');
-    emailInput.classList.add('success');
-    commentsInput.classList.add('success');
-    inputsContainer.classList.add('success');
-
-    try {
-      const response = await fetch(
-        'https://portfolio-js.b.goit.study/api-docs',
-        {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({ email, comments }),
-        }
-      );
-
-      if (response.ok) {
-        form.reset();
-        modal.style.display = 'flex';
-        modalOverlay.style.display = 'block';
-        disableBodyScroll(); 
-      } else {
-        alert('Error submitting form. Please check your input and try again.');
-      }
-    } catch (error) {
-      alert('Network error. Please try again later.');
-    }
-  } else {
-    validationText.textContent = 'Invalid email, try again';
+  // Mail format validation
+  if (!emailRegex.test(email)) {
+    validationText.textContent = 'Invalid email format';
     validationText.classList.add('error');
     emailInput.classList.add('error');
-    inputsContainer.classList.add('error');
+    return;
+  }
+
+  validationText.textContent = 'Success!';
+  validationText.classList.remove('error');
+  validationText.classList.add('success');
+
+  emailInput.classList.remove('error');
+  emailInput.classList.add('success');
+  commentsInput.classList.remove('error');
+  commentsInput.classList.add('success');
+  inputsContainer.classList.add('success');
+
+  try {
+    const response = await fetch('https://portfolio-js.b.goit.study/api-docs', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email, comments }),
+    });
+
+    if (response.ok) {
+      form.reset();
+      modal.style.display = 'flex';
+      modalOverlay.style.display = 'block';
+      disableBodyScroll();
+    } else {
+      alert('Error submitting form. Please check your input and try again.');
+    }
+  } catch (error) {
+    alert('Network error. Please try again later.');
   }
 });
 
@@ -72,7 +74,7 @@ function closeModal() {
   modal.style.display = 'none';
   modalOverlay.style.display = 'none';
   enableBodyScroll();
-  
+
   validationText.textContent = '';
   validationText.classList.remove('success');
   emailInput.classList.remove('success');
@@ -101,12 +103,11 @@ emailInput.addEventListener('input', () => {
 });
 
 commentsInput.addEventListener('input', () => {
-validationText.classList.remove('error');  
-validationText.textContent = ''; 
-commentsInput.classList.remove('error');
-inputsContainer.classList.remove('error');
+  validationText.textContent = '';
+  validationText.classList.remove('error');
+  commentsInput.classList.remove('error');
+  inputsContainer.classList.remove('error');
 });
-
 
 emailInput.addEventListener('blur', () => {
   emailInput.classList.remove('error');
@@ -115,8 +116,7 @@ emailInput.addEventListener('blur', () => {
 });
 
 commentsInput.addEventListener('blur', () => {
-  commentsInput.classList.remove('error');  
+  commentsInput.classList.remove('error');
   inputsContainer.classList.remove('error');
   validationText.classList.remove('error');
 });
-
